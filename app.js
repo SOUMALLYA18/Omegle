bd8c1c3c02e2da7def84ef07e6069170af5c49f5
SOUMALLYA MUKHERJEE <soumallya.india@gmail.com>
2024-12-24 13:24:54 +0530

    mesaging setup

--- app.js
+++ app.js
@@ -8,8 +8,33 @@ const socketIo = require("socket.io");
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let waitingUsers = [];
+let rooms = {};
+
 io.on("connection", function (socket) {
-  console.log("connected to web browser");
+  socket.on("joinroom", () => {
+    if (waitingUsers.length > 0) {
+      let partner = waitingUsers.shift();
+      const roomname = `${socket.id}-${partner.id}`;
+      socket.join(roomname);
+      partner.join(roomname);
+
+      io.to(roomname).emit("joined", roomname);
+    } else {
+      waitingUsers.push(socket);
+    }
+  });
+
+  socket.on("message", (data) => {
+    socket.broadcast.to(data.room).emit("message", data.message);
+  });
+
+  socket.on("disconnect", () => {
+    let index = waitingUsers.findIndex(
+      (waitingUser) => waitingUser.id === socket.id
+    );
+    waitingUsers.splice(index, 1);
+  });
 });
 
 app.set("view engine", "ejs");
